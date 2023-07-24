a18e0e03d0f5880d2692991b9329dd43b9080c61
wmarcolino <wallacemarcolino@gmail.com>
2023-07-24 20:35:59 -0300

    incluindo arquivo de pre cadastro

---
+++ cypress/e2e/pre-cadastro.spec.cy.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+import { faker } from "@faker-js/faker";
+
+describe("Funcionalidade Pre Cadastro", () => {
+  beforeEach(() => {
+    cy.visit("http://lojaebac.ebaconline.art.br/minha-conta/");
+  });
+
+  it("Deve completar o pré cadastro com sucesso", () => {
+    cy.get("#reg_email").type(faker.internet.email());
+    cy.get("#reg_password").type("!qatest2");
+    cy.get(":nth-child(4) > .button").click();
+
+    cy.get(".woocommerce-MyAccount-navigation-link--edit-account > a").click();
+    cy.get("#account_first_name").type(faker.name.firstName());
+    cy.get("#account_last_name").type(faker.name.lastName());
+    cy.get(".woocommerce-Button").click();
+
+    cy.get(".woocommerce-message").should(
+      "contain",
+      "Detalhes da conta modificados com sucesso"
+    );
+  });
+});
